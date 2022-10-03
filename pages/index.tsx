efd79dabcb26fb688f719fe469fc5b081f546079
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-03 21:13:57 +0700

    feat: add bottom hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,6 +143,26 @@ export default function HomePage() {
     </section>
   );
 
+  const bottomHeroSection = (
+    <section className={styles.bottomHeroSection}>
+      <div className={styles.container}>
+        <article>
+          <h2>םירבחל םיצילממ MOK-ב םיחוורמו</h2>
+          <p>
+            ,םינוק םה םאו םירבחל םיצילממ !לקו טושפ ךכ לכ םתרבצש ףסכב שמתשהל
+            ולכות םיחוורמ םתא !רתאב סרוק לכ תיינקל
+          </p>
+          <Button>וישכע חיוורהל וליחתת</Button>
+        </article>
+        <img
+          src="/assets/index-page/bottom-hero-image.png"
+          alt="student"
+          className={styles.bottomHeroImage}
+        />
+      </div>
+    </section>
+  );
+
   return (
     <main>
       {meta}
@@ -203,6 +223,7 @@ export default function HomePage() {
 
       {testimonySection}
       {howItWorksSection}
+      {bottomHeroSection}
     </main>
   );
 }
