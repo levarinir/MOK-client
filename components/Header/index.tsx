f6f0aa05dfad6ce66867894a8a4151d61eff7734
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-03 08:35:32 +0700

    refactor: Header component change css to scss

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,33 +1,33 @@
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 import { Bars3Icon, ShoppingCartIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className={styles["header"]}>
-      <Link href="/">
-        <a>
-          <Image
-            src="/static/images/header/logo.png"
-            width={70}
-            height={42}
-            alt="MOK logo"
-          />
-        </a>
-      </Link>
-
-      <div className={styles["header__menu"]}>
-        <button
-          className={styles["header__menu_cart_button"]}
-          // className="h-11 w-11 p-2 text-custom-secondary"
-        >
-          <ShoppingCartIcon></ShoppingCartIcon>
-        </button>
-
-        <button className={styles["header__menu_nav_toggle_button"]}>
-          <Bars3Icon />
-        </button>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Link href="/">
+          <a>
+            <Image
+              src="/static/images/header/logo.png"
+              width={70}
+              height={42}
+              alt="MOK logo"
+            />
+          </a>
+        </Link>
+        <div className={styles.menu}>
+          <button
+            className={styles.cartButton}
+            // className="h-11 w-11 p-2 text-custom-secondary"
+          >
+            <ShoppingCartIcon></ShoppingCartIcon>
+          </button>
+          <button className={styles.navToggleButton}>
+            <Bars3Icon />
+          </button>
+        </div>
       </div>
     </header>
   );
