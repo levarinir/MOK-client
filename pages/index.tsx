624d978d64b4b5d3d056c052301010229e0f8974
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-04 17:21:22 +0700

    feat: add another section

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,6 +107,21 @@ export default function HomePage() {
     </section>
   );
 
+  const howToEarnPointsSection = (
+    <section className={styles.howToEarnPointsSection}>
+      <div className={styles.container}>
+        <h2>תהפכו להיות MOK-ים ותרוויחו</h2>
+        <article>
+          <h3>גלו איך צוברים נקודות למימוש </h3>
+          <p>
+            תמליצו לחברים על הקורסים שאהבתם ותוכלו לצבור ולממש את הסכום לקניית
+            כל קורס שתבחרו באתר
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+
   const howItWorksSection = (
     <section className={styles.howItWorkSection}>
       <div className={styles.container}>
@@ -227,6 +242,7 @@ export default function HomePage() {
       <Header />
       {subHeader}
       {hero}
+      {howToEarnPointsSection}
 
       <CoursesSection
         sectionTitle="קורסי תואר ראשון"
