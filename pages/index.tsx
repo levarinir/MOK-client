90d36a012c64fa90ebd105e0ff869fc7881e80f9
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-04 20:27:04 +0700

    feat: add section

--- pages/index.tsx
+++ pages/index.tsx
@@ -91,6 +91,59 @@ export default function HomePage() {
     </section>
   );
 
+  const courseTypesSection = (
+    <section className={styles.courseTypesSection}>
+      <div className={styles.container}>
+        <h2>מבחר ענק של קורסים אונליין</h2>
+        <p>דומלל וצרתש םוחת ורחבת</p>
+        <div className={styles.tabsOuter}>
+          <div className={styles.tabs}>
+            <div className={styles.tabItem}>
+              <button>תואר ראשון</button>
+              <div className={styles.tabIsSelectedIndicator}></div>
+            </div>
+            <div className={styles.tabItem}>
+              <button>מכינות קדם אקדמיות</button>
+            </div>
+            <div className={styles.tabItem}>
+              <button> הקיטמתמב תויורגב</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.contentOuter}>
+          <div className={styles.contentInner}>
+            <article>
+              <p className={styles.title}>קורסים של תואר ראשון</p>
+              <p className={styles.description}>
+                בחלק של מכינות אקדמיות באתר MOK תמצאו קורסים החופפים לקורסי ליבה
+                הנלמדים בתואר הראשון למדעים מדוייקים או תארי הנדסה וכוללים
+                קורסים כמו אלגברה 1מ, אלגברה לינארית, חדוא 1, חדוא 2, אינפי
+                ועוד. הקורסים נועדו כדי להקל על סטודנטים לתואר ראשון להנגיש
+                תכנים הנלמדים באוניברסיטאות ומכללות בצורה של שיעורים מוקלטים,
+                מתוייגים ומסודרים אשר מועברים בהתאם לתכניות לימוד של מוסדות
+                להשכלה גבוהה ונלמדים ע”י מרצים בכירים בתעשייה כמו טכניון
+                ואוניברסיטת בן גוריון.
+              </p>
+            </article>
+          </div>
+        </div>
+        <div className={styles.courses}>
+          <article>
+            <header>
+              <p>טכניון</p>
+            </header>
+            <main>
+              <ul>
+                <li>מכינת מתמטיקה - תואר שני</li>
+              </ul>
+            </main>
+            <Button>צפייה בכל הקורסים</Button>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+
   const testimonySection = (
     <section className={styles.testimonySection}>
       <div className={styles.container}>
@@ -242,6 +295,7 @@ export default function HomePage() {
       <Header />
       {subHeader}
       {hero}
+      {courseTypesSection}
       {howToEarnPointsSection}
 
       <CoursesSection
