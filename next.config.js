a9233a3fb599c545096e241fdece69e68a02b2e7
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-06 14:10:11 +0700

    feat: add placeholder domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   publicRuntimeConfig: {
-    APP_NAME: 'NODE-REACT-AWS',
-    API: 'http://localhost:8000/api',
+    APP_NAME: "NODE-REACT-AWS",
+    API: "http://localhost:8000/api",
     PRODUCTION: false,
-    DOMAIN: 'http://localhost:3000',
-    FB_APP_ID: 'JJSLKADFLKSAHFDSLKL',
+    DOMAIN: "http://localhost:3000",
+    FB_APP_ID: "JJSLKADFLKSAHFDSLKL",
+  },
+  images: {
+    // TODO: remove picsum.photos when placeholders are no longer being used
+    domains: ["picsum.photos"],
   },
 };
