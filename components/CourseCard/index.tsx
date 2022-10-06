e7bc55fce20cdace1a4922bee8c2a192d83e7c46
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-06 14:15:39 +0700

    refactor: change card image to use <Image />

--- components/CourseCard/index.tsx
+++ components/CourseCard/index.tsx
@@ -2,6 +2,7 @@ import styles from "./CourseCard.module.scss";
 import { PlayCircleIcon } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import Image from "next/image";
 
 export interface CourseCardProps {
   cardImageSrc: string;
@@ -20,7 +21,7 @@ function CourseCard(props: CourseCardProps) {
     <Link href={props.href}>
       <a>
         <article className={styles.courseCard}>
-          <img src={props.cardImageSrc} alt={props.title} />
+          <Image width="100%" height={146} src={props.cardImageSrc} alt={props.title} />
           <p className={styles.title}>{props.title}</p>
           <p className={styles.description}>{props.description}</p>
           <p className={styles.duration}>
